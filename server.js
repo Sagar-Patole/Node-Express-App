6225f3010d41679f859c0fc91ebac6532a33b5b7
Sagar Patole <sagapato@amdocs.com>
2024-08-01 17:49:58 +0530

    Adding Sequelize with MySql Database

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const rootDir = require('./utils/path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
+const sequelize = require('./utils/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -16,9 +23,51 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(rootDir, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1).then(user => {
+        req.user = user;
+        next();
+    }).catch(error => {
+        console.log(error);
+    });
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem});
+
+
+sequelize.sync().then(result => {
+    return User.findByPk(1).then(user => {
+        if (!user) {
+            return User.create({name: 'Sagar', email: 'test@test.com'});
+        }
+        return user;
+    }).catch(e => {
+        console.log(e);
+    });
+}).then(user => {
+    return user.getCart().then(cart => {
+        if (!cart) {
+            return user.createCart();
+        }
+        return cart;
+    }).catch(err => {
+        console.log(err);
+    });
+}).then(cart => {
+    app.listen(3000);
+}).catch(error => {
+    console.log(error);
+});
--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    database: 'express-app',
-    user: 'root',
-    password: 'root'
+const sequelize = new Sequelize('express-app', 'root', 'root', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
